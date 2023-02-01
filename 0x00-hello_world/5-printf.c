5583ad09600bccf1726164f19c5fd8bc19de8026
TPoe25 <5565@holbertonstudents.com>
2023-02-01 15:38:33 -0800

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
+/*
+ * main- Entry Point
+ *
+ * description: printf
+ *
+ * return: Always 0
+ */
+
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
