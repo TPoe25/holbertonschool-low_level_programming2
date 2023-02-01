3a0671e1febfe024aa7d52921d029864984d3a08
TPoe25 <5565@holbertonstudents.com>
2023-01-31 22:19:30 -0800

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,12 @@
 #include <string.h>
 
 int main () {
-	   char str1[52];
-	      char str2[52];
+	char str1[52];
+	 char str2[52];
 
-	         strcpy(str1, "Programming is like building a multilingual puzzle\n");
+ 	 strcpy(str1, "Programming is like building a multilingual puzzle\n");
 
-		    puts(str1);
+	 puts(str1);
 		       
-		       return(0);
+         return(0);
 }
