022852c7f096163b4cf6a3816b819d5bdf62bf47
TPoe25 <5565@holbertonstudents.com>
2023-02-01 16:20:41 -0800

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,13 +13,12 @@ int main(void)
 	int i;
 	long int l;
 	long long int r;
-	float f;
+	float j;
 
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(i));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(l));
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(r));
-	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
-
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(j));
 	return (0);
 }
