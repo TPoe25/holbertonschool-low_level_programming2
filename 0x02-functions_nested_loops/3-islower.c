210776fcfb02080cf0a26887def5f21782b67dab
TPoe25 <t_poe25@yahoo.com>
2023-02-19 21:28:41 -0800

    3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,10 +1,10 @@
 #include "main.h"
+
 /**
  * int _itslower(int c ) - checks for lowercase letters
  *
  * return: 1 for lowercase characters, 0 if else
  */
-
 int _itslower(int c)
 {
 	if (c >= 'a' && c <= 'z')
